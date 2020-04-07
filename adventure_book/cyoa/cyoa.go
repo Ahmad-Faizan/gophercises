b54306db67bd380216cf28cd84e020e8d400619a
Faizan Ahmad <faizan.nasro@gmail.com>
2020-04-07 23:25:22 +0530

    Added styling to the page

--- adventure_book/cyoa/cyoa.go
+++ adventure_book/cyoa/cyoa.go
@@ -27,22 +27,63 @@ const tmpl = `<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Choose Your Own Adventure</title>
+	<title>Choose Your Own Adventure</title>
+	<style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-bottom: 40px;
+		padding: 50px;
+		padding-top: 5px;
+        background: #fff9c4;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #777;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: none;
+        color: #6295b5;
+      }
+      a:active,
+      a:hover {
+        color: #f7a2a2;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
 </head>
 <body>
-    <h1>{{.Title}}</h1>
-    <div>
-        {{range .Paragraphs}}
-            <p>{{.}}</p>
-        {{end}}
-    </div>
-    <div>
-        {{range .Options}}
-            <ul>
-                <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-            </ul>
-        {{end}}
-    </div>
+	<section class="page">
+		<h1>{{.Title}}</h1>
+		<div>
+			{{range .Paragraphs}}
+				<p>{{.}}</p>
+			{{end}}
+		</div>
+		<div>
+			{{range .Options}}
+				<ul>
+					<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+				</ul>
+			{{end}}
+		</div>
+	</section>
 </body>
 </html>`
 
